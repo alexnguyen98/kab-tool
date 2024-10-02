accade849e4dfbc062d214cdb0c7265401ba235f
alexnguyen98 <alexnguyen98@yahoo.com>
2024-10-02 16:11:17 +0700

    update: packages

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import Text from '../components/common/Text';
-import TextArea from './common/TextArea';
-import { useGlobalContext } from '../context/ManagedContext';
+import React from "react";
+import Text from "../components/common/Text";
+import { useGlobalContext } from "../context/ManagedContext";
+import TextArea from "./common/TextArea";
 
 const Results: React.FC = () => {
   const { input, setInput, output, setOutput } = useGlobalContext();
@@ -16,17 +16,26 @@ const Results: React.FC = () => {
 
   return (
     <div className="flex-1 flex flex-col border-r-2 border-accent-2">
-      <div className="flex-1 flex flex-col border-t-2 border-accent-2">
+      <div className="flex-1 flex flex-col border-b-2 border-accent-2">
         <Text variant="small" className="p-3 bg-accent-1">
           Input
         </Text>
-        <TextArea className="flex-1 px-3 focus:outline-none" value={input} onChange={handleInput} />
+        <TextArea
+          className="flex-1 px-3 focus:outline-none"
+          value={input}
+          onChange={handleInput}
+        />
       </div>
       <div className="flex-1 flex flex-col">
-        <Text variant="small" className="p-3 bg-accent-1">
+        <Text variant="small" className="p-3">
           Output
         </Text>
-        <TextArea disabled className="flex-1 px-3 bg-white cursor-not-allowed" value={output?.toLowerCase()} onChange={handleOutput} />
+        <TextArea
+          disabled
+          className="flex-1 px-3 bg-white cursor-not-allowed"
+          value={output?.toLowerCase()}
+          onChange={handleOutput}
+        />
       </div>
     </div>
   );
